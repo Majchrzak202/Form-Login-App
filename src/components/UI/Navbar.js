ec029c448454f0776dbf72aa1283588322d0ed90
MarcinMaster <m.luba505@gmail.com>
2022-05-19 07:07:25 +0200

    Adding a Navbar Component and CSS styles

---
+++ src/components/UI/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+      <div className='navbar'>
+          <img src="https://cryptologos.cc/logos/bitcoin-cash-bch-logo.png?v=022" className='logo' alt='logo'/>
+          <p className='text-block'> UserName</p>
+          <img src="https://cryptologos.cc/logos/shiba-inu-shib-logo.png?v=022" className='user-img' alt='userLogo'></img>
+          <button classname='loginBtn'> LOGIN </button>
+      </div>
+  )
+};
+
+export default Navbar;
