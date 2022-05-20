b400af7a2a868f4684c1c9657300cb94f0e79b7f
MarcinMaster <m.luba505@gmail.com>
2022-05-20 06:25:50 +0200

    Adding a Footer to make it look great

---
+++ src/components/UI/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css'
+
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p className='paragraph'>Copyright by Happy Hackers!</p>
+        </div>
+    )
+}
+
+
+export default Footer;
