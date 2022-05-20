86c629e677748aa3fb7811daed97b1a39c5a494f
MarcinMaster <m.luba505@gmail.com>
2022-05-20 06:23:08 +0200

    Adding isSubmited state to display conditional content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-
-import './App.css';
-import Navbar from './components/UI/Navbar';
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./components/UI/Navbar";
+import Header from "./components/UI/Header";
+import AddUserForm from "./components/Form/AddUserForm";
+import Footer from "./components/UI/Footer";
 
 function App() {
+  const [isSubmited, setIsSubmited] = useState(false);
+
+  const saveUserData = (data) => {
+    fetch(
+      "https://login-form-test-project-default-rtdb.europe-west1.firebasedatabase.app/users.json",
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
+  };
+
   return (
     <div className="App">
-     <Navbar/>
+      <Navbar />
+      {(isSubmited) ? null : <Header />}
+      <AddUserForm isSubmited={isSubmited} setIsSubmited={setIsSubmited} saveUserData={saveUserData} />
+      <Footer />
     </div>
   );
 }
