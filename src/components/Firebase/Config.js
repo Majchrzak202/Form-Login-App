8a6eba0b0544834d43bd9b7d87c892eade464315
MarcinMaster <m.luba505@gmail.com>
2022-06-05 06:50:22 +0200

    Added a firebase config and install FirebaseUI

--- src/components/Firebase/Config.js
+++ src/components/Firebase/Config.js
@@ -1,5 +1,16 @@
+import { firebase, initializeApp } from 'firebase/app';
+import firebaseui from 'firebaseui';
 
 
-const firebaseCofig = {
-    
-}
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAviin84mdNM-6vdNeLRd_R646XhsKUpfk",
+    authDomain: "login-form-app-profile-picture.firebaseapp.com",
+    projectId: "login-form-app-profile-picture",
+    storageBucket: "login-form-app-profile-picture.appspot.com",
+    messagingSenderId: "797860966461",
+    appId: "1:797860966461:web:54e5a10ec2534f33f43a54"
+  };
+
+const app = initializeApp(firebaseConfig);
+const ui = new firebaseui.auth.AuthUI(firebase.auth())
