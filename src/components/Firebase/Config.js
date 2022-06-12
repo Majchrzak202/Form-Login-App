482e95646f5200503536362807b0ca81d533ac50
MarcinMaster <m.luba505@gmail.com>
2022-06-12 07:54:36 +0200

    Added some minor changes

--- src/components/Firebase/Config.js
+++ src/components/Firebase/Config.js
@@ -14,3 +14,11 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ]
+  });
+
+
