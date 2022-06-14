aba7d1b37fce119e6e48c391f2357e12afed3b86
MarcinMaster <m.luba505@gmail.com>
2022-06-14 19:55:21 +0200

    Added a useForm hook to outsource the logic of form validation, reset and make a codebase clean and lean

---
+++ src/components/hooks/useForm.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const useForm = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (e) => {
+    setEnteredValue(e.target.value);
+  };
+
+  const valueInputBlurHandler = () => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    hasError,
+    enteredValue,
+    valueIsValid,
+    reset,
+    valueInputBlurHandler,
+    valueChangeHandler
+};
+};
+
+export default useForm;
