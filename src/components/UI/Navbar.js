9961e8a041235660cc91afc75cedb3459165f057
MarcinMaster <m.luba505@gmail.com>
2022-06-26 06:31:18 +0200

    Added login/logout conditional in the navbar

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,30 +1,37 @@
 import React from "react";
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
-/* import LoginButton from "../Auth0/LoginButton";
-import LogoutButon from "../Auth0/LogoutButton"; */
+import { useAuth } from "../context/UserAuthContextProvider";
 
-const Navbar = ({ isAuthenticated, user }) => {
+const Navbar = () => {
   const navigate = useNavigate();
+  const { logout, user } = useAuth();
 
   const loginHandler = (e) => {
     e.preventDefault();
     navigate("/login");
   };
 
+  const logoutHandler = (e) => {
+    e.preventDefault();
+    logout()
+    navigate('/login')
+  };
+
   return (
     <div className="navbar">
-      {isAuthenticated && <p className="text-block"> {user.name}</p>}
       <img
         src="https://cryptologos.cc/logos/shiba-inu-shib-logo.png?v=022"
         className="user-img"
         alt="userLogo"
       ></img>
-      <button onClick={loginHandler} className="button">
+      { !user ? (<button onClick={loginHandler} className="button">
         LOGIN
-      </button>
-      {/*   <LoginButton />
-      <LogoutButon /> */}
+      </button>) :
+      (<button onClick={logoutHandler} className="button">
+        LOGOUT
+      </button>)}
+      
     </div>
   );
 };
